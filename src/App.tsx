2e0c93682958b6292df477fe072e6c2a572fc23b
bhanuu7 <bhanuthanneeru411@gmail.com>
2026-01-04 15:09:22 +0530

    Tanstack, axios integration and login API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,26 @@
-import { useState } from 'react';
+import { useEffect } from 'react';
+import { login } from './api/auth';
+import { useMutation } from '@tanstack/react-query';
 function App() {
-  return <div>Hello world</div>;
+  const mutation = useMutation({
+    mutationFn: login,
+    retry: 1,
+    onSuccess(data, variables, onMutateResult, context) {
+      console.log('mutation success');
+    },
+  });
+  const handleSubmit = () => {
+    mutation.mutate({
+      email: 'bhanu@gmail.com',
+      password: '123456',
+    });
+  };
+  return (
+    <div>
+      <h5>Hello world</h5>
+      <button onClick={handleSubmit}>Submit</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/api/auth.ts
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+export interface AuthError extends Error {
+  status: number;
+}
+
+interface Payload {
+  email: string;
+  password: string;
+}
+
+export const login = async ({ email, password }: Payload): Promise<any> => {
+  const response = await axios.get('http://localhost:3000/users', {
+    params: { email, password },
+  });
+  const user = response.data[0];
+  if (!user) {
+    const error = new Error('Invalid username or password') as AuthError;
+    error.status = 401;
+  }
+  return user;
+};
